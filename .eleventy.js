017ed1660852c22e6880bd67b49b1e0a70405ffc
bradleycorrigan <73534576+bradleycorrigan@users.noreply.github.com>
2025-04-01 22:12:47 +0100

    add sidebar

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
-module.exports = function
-(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/style.css");  
+module.exports = function(eleventyConfig) {
+    // Ensure Eleventy detects Markdown posts
+    eleventyConfig.addCollection("posts", function(collectionApi) {
+        return collectionApi.getFilteredByGlob("src/**/*.md");
+    });
+
+    // Pass through CSS
+    eleventyConfig.addPassthroughCopy("src/style.css");
 
     return {
         dir: {
-            input: "src",
-            output: "docs"
+            input: "src",   // Source directory
+            output: "docs"  // Build output directory
         }
     };
-}
+};
